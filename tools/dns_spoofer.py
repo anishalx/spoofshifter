92843937d1295750e4cfd56a5ebefd7296782470
Anish <113669810+anishalx@users.noreply.github.com>
2024-09-14 23:45:45 +0530

    Update dns_spoofer.py

--- tools/dns_spoofer.py
+++ tools/dns_spoofer.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env pyhthon
+#requirement pip install netfilterqueue
 
+import netfilterqueue
+
+
+
+def process_packet(packet):
+    print(packet)
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
